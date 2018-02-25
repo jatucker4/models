a139bac036377b708c70a8a8fca682299753a23b
jatucker4 <31327486+jatucker4@users.noreply.github.com>
2018-02-24 20:18:43 -0700

    Update wide_deep.py

--- official/wide_deep/wide_deep.py
+++ official/wide_deep/wide_deep.py
@@ -24,14 +24,10 @@ import sys
 import tensorflow as tf
 
 _CSV_COLUMNS = [
-    'age', 'workclass', 'fnlwgt', 'education', 'education_num',
-    'marital_status', 'occupation', 'relationship', 'race', 'gender',
-    'capital_gain', 'capital_loss', 'hours_per_week', 'native_country',
-    'income_bracket'
+    'Sentiment', 'Magnitude', 'Favorites', 'ReTweets'
 ]
 
-_CSV_COLUMN_DEFAULTS = [[0], [''], [0], [''], [0], [''], [''], [''], [''], [''],
-                        [0], [0], [0], [''], ['']]
+_CSV_COLUMN_DEFAULTS = [[0], [0], [0], [0]]
 
 parser = argparse.ArgumentParser()
 
@@ -70,68 +66,34 @@ _NUM_EXAMPLES = {
 def build_model_columns():
   """Builds a set of wide and deep feature columns."""
   # Continuous columns
-  age = tf.feature_column.numeric_column('age')
-  education_num = tf.feature_column.numeric_column('education_num')
-  capital_gain = tf.feature_column.numeric_column('capital_gain')
-  capital_loss = tf.feature_column.numeric_column('capital_loss')
-  hours_per_week = tf.feature_column.numeric_column('hours_per_week')
-
-  education = tf.feature_column.categorical_column_with_vocabulary_list(
-      'education', [
-          'Bachelors', 'HS-grad', '11th', 'Masters', '9th', 'Some-college',
-          'Assoc-acdm', 'Assoc-voc', '7th-8th', 'Doctorate', 'Prof-school',
-          '5th-6th', '10th', '1st-4th', 'Preschool', '12th'])
-
-  marital_status = tf.feature_column.categorical_column_with_vocabulary_list(
-      'marital_status', [
-          'Married-civ-spouse', 'Divorced', 'Married-spouse-absent',
-          'Never-married', 'Separated', 'Married-AF-spouse', 'Widowed'])
-
-  relationship = tf.feature_column.categorical_column_with_vocabulary_list(
-      'relationship', [
-          'Husband', 'Not-in-family', 'Wife', 'Own-child', 'Unmarried',
-          'Other-relative'])
-
-  workclass = tf.feature_column.categorical_column_with_vocabulary_list(
-      'workclass', [
-          'Self-emp-not-inc', 'Private', 'State-gov', 'Federal-gov',
-          'Local-gov', '?', 'Self-emp-inc', 'Without-pay', 'Never-worked'])
-
-  # To show an example of hashing:
-  occupation = tf.feature_column.categorical_column_with_hash_bucket(
-      'occupation', hash_bucket_size=1000)
-
+  sentiment = tf.feature_column.numeric_column('sentiment')
+  magnitude = tf.feature_column.numeric_column('magnitude')
+  favorites = tf.feature_column.numeric_column('favorites')
+  retweets = tf.feature_column.numeric_column('retweets')
+  
   # Transformations.
-  age_buckets = tf.feature_column.bucketized_column(
-      age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
+  sentiment_buckets = tf.feature_column.bucketized_column(
+      sentiment, boundaries=[-1, -0.5, 0, 0.5, 1]
 
   # Wide columns and deep columns.
   base_columns = [
-      education, marital_status, relationship, workclass, occupation,
-      age_buckets,
+      sentiment_buckets, magnitude, favorites, retweets
   ]
 
   crossed_columns = [
       tf.feature_column.crossed_column(
-          ['education', 'occupation'], hash_bucket_size=1000),
+          [magnitude, sentiment_buckets], hash_bucket_size=1000),
       tf.feature_column.crossed_column(
-          [age_buckets, 'education', 'occupation'], hash_bucket_size=1000),
+          [sentiment_buckets, retweets, favorites], hash_bucket_size=1000),
   ]
 
   wide_columns = base_columns + crossed_columns
 
   deep_columns = [
-      age,
-      education_num,
-      capital_gain,
-      capital_loss,
-      hours_per_week,
-      tf.feature_column.indicator_column(workclass),
-      tf.feature_column.indicator_column(education),
-      tf.feature_column.indicator_column(marital_status),
-      tf.feature_column.indicator_column(relationship),
-      # To show an example of embedding
-      tf.feature_column.embedding_column(occupation, dimension=8),
+      sentiment,
+      magnitude,
+      favorites,
+      retweets,
   ]
 
   return wide_columns, deep_columns
@@ -177,8 +139,8 @@ def input_fn(data_file, num_epochs, shuffle, batch_size):
     print('Parsing', data_file)
     columns = tf.decode_csv(value, record_defaults=_CSV_COLUMN_DEFAULTS)
     features = dict(zip(_CSV_COLUMNS, columns))
-    labels = features.pop('income_bracket')
-    return features, tf.equal(labels, '>50K')
+    labels = features.pop('Favorites')
+    return features, tf.equal(labels, '>10')
 
   # Extract lines from input files using the Dataset API.
   dataset = tf.data.TextLineDataset(data_file)
